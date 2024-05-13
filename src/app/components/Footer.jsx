df9126a30eabf444a6a042830748ab3ecf31804d
The-Shy7 <tahmintalukder98@gmail.com>
2024-05-13 14:06:06 -0700

    first commit, implemented navbar and footer components

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,22 @@
+const Footer = () => {
+  return (
+    <>
+        <hr/>
+        <div className="footer-container">
+            <p>© {new Date().getFullYear()} Tahmin's Portfolio</p>
+            <div className="social-icons">
+                <a
+                    href="https://github.com/The-Shy7"
+                    aria-label="GitHub"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <i className="fab fa-github"></i>
+                </a>
+            </div>
+        </div>
+    </>
+  );
+}
+
+export default Footer;
