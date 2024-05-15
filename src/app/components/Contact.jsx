aa0673e7b12e24826afd31cc634da6c59090d4a3
The-Shy7 <tahmintalukder98@gmail.com>
2024-05-14 21:47:35 -0700

    finished contact component, need to finalize content and check styling

---
+++ src/app/components/Contact.jsx
@@ -0,0 +1,11 @@
+const Contact = () => {
+  return (
+    <div className="contact-container">
+        <h2>Get In Touch</h2>
+        <p>Feel free to reach out to me anytime for new opportunities! My inbox is always open. I'll try my best to get back to you! Cheers!</p>
+        <a href="mailto:tahmintalukder98@gmail.com" className='cta-btn'>Say Hello</a>
+    </div>
+  );
+}
+
+export default Contact;
--- src/app/page.js
+++ src/app/page.js
@@ -4,17 +4,26 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Tahmin's Portfolio</title> 
+        <meta name="description" content="Tahmin's Portfolio" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <Navbar />
-      <div>
-        <Hero />
-        <About />
-        <Skills />
-        <Projects />
-      </div>
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
       <Footer />
     </>
   );
